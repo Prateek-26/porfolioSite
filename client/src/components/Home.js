b9a63078469f3417547d65d52eb1aebb6f89d328
Prateek Sharma <sharmaprateek.dev@gmail.com>
2023-04-15 19:36:13 +0530

    Home page created

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,11 +2,16 @@ import React from "react";
 
 function Home(){
     return(
-        <div>
-            <p className="pt-5">WELCOME</p>
-            <h1>We are the MERN Developers</h1>
-        </div>
+        <>
+            <section className="home-container">
+                <h4>Welcome</h4>
+                <h1>We Are MERN Developers</h1>
+                <div className="row home-bg">
+                <div className="col-sm-6 left"></div>
+                <div className="col-sm-6 right"></div>
+                </div>
+            </section>
+        </>
     )
 }
-
 export default Home;
