f3bcf36698bb9f6c745a1ca8a19a3e2120a97b8f
Prateek Sharma <sharmaprateek.dev@gmail.com>
2023-04-15 13:53:55 +0530

    Contact page completed

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,10 +2,9 @@ import React from "react";
 
 function About(){
     return(
-        <div>
-            <p className="pt-5">WELCOME To About</p>
-            <h1>We are the MERN Developers</h1>
-        </div>
+        <>
+            
+        </>
     )
 }
 
--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -2,10 +2,49 @@ import React from "react";
 
 function Contact(){
     return(
-        <div>
-            <p className="pt-5">WELCOME To Contact</p>
-            <h1>We are the MERN Developers</h1>
-        </div>
+        <>
+            <section className="contact-page">
+            <div className="container-fluid contact-container">
+                <div className="row contact-container-items">
+                <div className="col-sm-3 contact-container-item">
+                    <div className="contact-item-title">Phone</div>
+                    <div className="contact-item-content">9096091326</div>
+                </div>
+                <div className="col-sm-3 contact-container-item">
+                <div className="contact-item-title">Email</div>
+                    <div className="contact-item-content">test@gmail.com</div>                   
+                </div>
+                <div className="col-sm-3 contact-container-item">
+                <div className="contact-item-title">Address</div>
+                    <div className="contact-item-content">Goa, India</div>
+                </div>
+                </div>
+            </div>
+
+            <div className="get-in-touch-container">
+                <div className="get-in-touch-content">
+                <h1>Get In Touch</h1>
+                <form action="">
+                <div className="row contact-container-items">
+
+                                <input type="text" class="form-control shadow-none col-sm-2" id="name" placeholder="Your Name" name="name" required  />
+
+                                <input type="email" class="form-control shadow-none col-sm-2" placeholder="Your Email" name="email" required  />
+                        
+                                <input type="number" class="form-control shadow-none col-sm-2" id="phone" placeholder="Your Mobile Number" name="phone" required />
+
+                    </div>
+            <div className="row mssg-container">
+                <textarea className="col-12" name="" id="" cols="30" rows="10" placeholder="Your Message"></textarea>
+                </div>
+            <button type="submit" className="btn btn-primary">Send Message</button>
+                </form>
+                </div>
+  
+            </div>
+            </section>
+            
+        </>
     )
 }
 
